9d94ae4c20749195ca3429bcb43243763caf8932
Khoja Naviz <20itnav068@ldce.ac.in>
2022-12-05 09:38:27 +0530

    added required packages

---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class constants {
+  static String appId = "1:27897709037:web:5cb97f3a8164dc21cee7e4";
+  static String apiKey = "AIzaSyDpPVv7AFhSbfY_KbdU5KikCeiCPu_wK9M";
+  static String messageSenderId= "vizchat-1c5a5";
+  static String projectId = "27897709037";
+}
